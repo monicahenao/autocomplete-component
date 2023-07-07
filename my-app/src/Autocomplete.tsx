9ab7a3afac8d97fc575ed0faad2a8436bed99ebd
monicahenao <60370417+monicahenao@users.noreply.github.com>
2023-07-07 16:27:26 +0000

    update1 autocomplete

---
+++ my-app/src/Autocomplete.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Autocomplete.css';
+
+function Autocomplete() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <input
+        />
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default Autocomplete;
